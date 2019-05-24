7427d7406cb54c415296ddcc27d02bd95ad1aff8
justine moreau <juhmoreau@gmail.com>
2019-05-24 12:57:22 +0200

    ok

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -17,9 +17,9 @@ class DosesController < ApplicationController
 
   def destroy
     @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
     @dose.destroy
     redirect_to cocktail_path(@cocktail)
-    #@garden = @plant.garden
   end
 
   def dose_params
